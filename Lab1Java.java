2fcfb23552daaa0ebce6f1b86b3bc63f8a07a96d
Hafiz Muhammad Ahtisham <ahtishamkamil67@gmail.com>
2025-10-21 10:19:29 +0500

    Add files via upload

---
+++ Lab1Java.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1.java;
+
+/**
+ *
+ * @author lab2
+ */
+public class Lab1Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int number = 7;
+        
+        System.out.println("Multiplication Table of 7:");
+        System.out.println("-------------------------");
+
+        for (int i = 1; i <= 10; i++) {
+            int product = number * i;
+            System.out.println(number + " x " + i + " = " + product);
+        }
+        // TODO code application logic here
+    }
+    
+}
